e06fd4c64ede0c62355ea4683a21bf0b7d659a36
Bruno Almeida <bruno@verdelog.com.br>
2025-06-23 15:28:25 -0300

    colocando @vercel/speed-insights/next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../styles/globals.css';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: 'Meu Portfólio',
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body className="bg-gray-900 text-white">
         <Header />
         <main>{children}</main>
+        <SpeedInsights />
         <Footer />
       </body>
     </html>
